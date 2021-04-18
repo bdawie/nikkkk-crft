242a2be12b921113dec6313a4eee32eef276c89f
bdawie <ibrahimbadivi@gmail.com>
2021-04-18 19:27:35 +0300

    use ngOnInit, add rxjs pipes, and apply some styles

--- src/app/components/date-time/date-time.component.ts
+++ src/app/components/date-time/date-time.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
-import { DateTimeService } from 'src/app/services/date-time.service';
+import { Component, OnInit } from "@angular/core";
+import { DateTimeService } from "src/app/services/date-time.service";
+
+import { map, tap } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'date-time',
-  templateUrl: './date-time.component.html',
+  selector: "date-time",
+  styles: [
+    `
+      .date-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: Helvetica;
+        border: 2px solid #c3c3c3;
+        width: 300px;
+        margin: 0 auto;
+      }
+    `,
+  ],
+  templateUrl: "./date-time.component.html",
 })
-export class DateTimeComponent {
-  date;
+export class DateTimeComponent implements OnInit {
+  date$: Observable<Date>;
 
-  timezone;
+  timezone: string;
 
-  constructor(private dateTimeService: DateTimeService) {
+  constructor(private dateTimeService: DateTimeService) {}
 
-    this.dateTimeService.getDateTimeInfo().subscribe(res => {
-      this.timezone = res.timezone;
-      this.date = new Date(res.datetime);
-
-    });
+  ngOnInit() {
+    this.date$ = this.dateTimeService.getDateTimeInfo().pipe(
+      tap((res) => (this.timezone = res.timezone)),
+      map((res) => {
+        return new Date(res.datetime);
+      })
+    );
   }
 }
